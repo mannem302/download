0771a67399f1ce8e4bfd016b17f8becf7d892504
mannem302 <102237426+mannem302@users.noreply.github.com>
2024-08-28 16:07:56 +0530

    Update init.groovy

--- init.groovy
+++ init.groovy
@@ -6,7 +6,7 @@ import hudson.security.*
 def instance = Jenkins.getInstance()
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("admin", “admin”)
+hudsonRealm.createAccount("admin", "admin")
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
