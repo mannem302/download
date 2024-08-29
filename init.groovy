bfdc19750e18d423b203c2cb9e648bfb64220514
mannem302 <102237426+mannem302@users.noreply.github.com>
2024-08-29 12:58:53 +0530

    Update init.groovy

--- init.groovy
+++ init.groovy
@@ -3,19 +3,7 @@
 import jenkins.model.*
 import hudson.security.*
 import jenkins.install.*
-import hudson.PluginManager
 import jenkins.model.JenkinsLocationConfiguration
-
-// Define the default plugins you want to install
-def plugins = [
-    'git', // Git plugin
-    'pipeline', // Pipeline plugin
-    'workflow-aggregator', // Workflow Aggregator plugin
-    'blueocean', // Blue Ocean plugin
-    'docker-workflow', // Docker Workflow plugin
-    'github', // GitHub plugin
-    'maven-plugin' // Maven Integration plugin
-]
 
 def instance = Jenkins.getInstance()
 
@@ -30,26 +18,3 @@ instance.setAuthorizationStrategy(strategy)
 // Save the configuration
 instance.save()
 
-// Install default plugins
-def pluginManager = instance.getPluginManager()
-def updateCenter = instance.getUpdateCenter()
-
-plugins.each { pluginName ->
-    def plugin = updateCenter.getPlugin(pluginName)
-    if (plugin && !plugin.isInstalled()) {
-        println "Installing ${pluginName} plugin..."
-        plugin.deploy()
-        pluginManager.installPlugin(plugin)
-    }
-}
-
-// Wait for plugins to be installed
-def waiting = true
-while (waiting) {
-    waiting = pluginManager.getPlugins().findAll { !it.isEnabled() }.size() > 0
-    sleep(5000)
-}
-
-// Restart Jenkins to apply changes
-println "Restarting Jenkins..."
-Jenkins.getInstance().restart()
